e3d888b58701933695829b321c15a6fc4844318f
Moktari Mostofa <44511241+TataMoktari@users.noreply.github.com>
2025-05-30 03:11:55 -0400

    Update WGDA_train.py

--- src/WGDA_train.py
+++ src/WGDA_train.py
@@ -95,7 +95,7 @@ shuffle_dataset = True
 
 # load data
 root_dir = "../data/49x49x17"
-train_data = pd.read_csv(root_dir + "/train_csv_files/high_dose_unnorm_train.csv")
+train_data = pd.read_csv(root_dir + "/train_csv_files/high_dose_train.csv")
 
 
 # IF 3-D DATASET
